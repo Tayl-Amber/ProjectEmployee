37b5415d994ec06d6e14e73fbc1adf145edcce77
Tayl_Amber <taylamber352@gmail.com>
2024-02-23 14:14:10 +0500

    Manager Class inherited

---
+++ Manager.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <iostream>
+#include "Employee.h"
+
+using namespace std;
+
+class Manager : public Employee
+{
+private:
+    float extraCap;
+
+public:
+    Manager(string name, int id, double salary, double extraCap = 5000.00) : Employee(name, id, salary), extraCap{extraCap}
+    {
+        cout << "Constructor for Manager Class Called!!" << endl;
+    }
+    bool managerSalary(double bonus);
+};
+
+bool Manager ::managerSalary(double bonus)
+{
+    return Employee ::calculateSalary(bonus + extraCap);
+}
