5cb777577cdd05fe1ffbd7f1acba7f8eb7b4f2a3
TaylAmber <taylamber352@gmail.com>
2024-04-09 13:57:42 +0500

    Utitlity Functions added

--- Choice.h
+++ Choice.h
@@ -11,17 +11,34 @@ using namespace std;
 class ChoiceInvoice
 {
 public:
+     void EmployeeActions();
+    void UserInputForPost(string name, int id, double salary);
     void askPosition();
-    void takeUserInput();
-    void askForAction();
+    Employee* askForAction(string name, int id, double salary, int choice);
 };
+
+void ChoiceInvoice::EmployeeActions(){
+    cout << "1. Calculate Salary" << endl;
+    cout << "2. Assign Projects" << endl;
+    cout << "4. Exit" << endl;
+}
+void ChoiceInvoice::UserInputForPost(string name, int id, double salary){
+     cout << "Enter your name: ";
+     cin.ignore();
+     getline(cin, name);
+
+     cout << "Enter your Employee Id: ";
+     cin >> id;
+
+     cout << "Enter your fixed Salary: ";
+     cin >> salary;
+}
 void ChoiceInvoice::askPosition(){
    cout << "Select Your Post: " << endl;
     cout << "1.Manger" << endl;
     cout << "2.Junior Developer" << endl;
     cout << "3.Intern" << endl;
 }
-void ChoiceInvoice::takeUserInput(string name, int id, double salary)
-{
+Employee* ChoiceInvoice::askForAction(string name, int id, double salary, int choice){
     
 }
